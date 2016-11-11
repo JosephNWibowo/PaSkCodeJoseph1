49b0af6e0ea3cbbf0bb6c78b414616b115f10d96
JosephNWibowo <JosephNWibowo@users.noreply.github.com>
2016-11-11 02:07:02 -0800

    uploading my file from the PaSk 11.4

---
+++ src/PaSkCode/PSysModel.java
@@ -0,0 +1,11 @@
+package PaSkCode;
+public class PSysModel {
+    // ArrayList of particles
+    // each particle: x, y, velX, velY, radius
+
+    PSysModel() {
+    }
+
+    void add(int rad, int x, int y, int vx, int vy) {
+    }
+}
---
+++ src/PaSkCode/PSysView.java
@@ -0,0 +1,12 @@
+package PaSkCode;
+import java.awt.Graphics;
+
+public class PSysView {
+
+    PSysView() {
+    }
+
+    void draw(PSysModel psm, Graphics g) {
+
+    }
+}
