2d9f8ad94ab34c4a8c22ef40ea8f65e2cb16820c
josephnwibowo <joeynathanwibowo@gmail.com>
2016-11-11 16:53:15 -0800

    particle just going right over and over

--- src/PaSkCode/PSysModel.java
+++ src/PaSkCode/PSysModel.java
@@ -1,29 +1,87 @@
 package PaSkCode;
 
+import java.util.ArrayList;
+
 public class PSysModel {
     // ArrayList or similar of particles
     // each particle: x, y, velX, velY, radius
 
-
+    public ArrayList<ParticleObj> par;
+    int index;
+    int count = 0;
 
     PSysModel() {
         // instantiate list of particles
+        par = new ArrayList<ParticleObj>();
+
     }
 
     // add a particle to list
     void add(int rad, int x, int y, int vx, int vy) {
 
-		radius = rad;
-		px = npx;
-		py = npy;
-		velX = nvx;
-		velY = nvy;
+        ParticleObj tempPar = new ParticleObj(rad, x, y, vx, vy);
 
-	    System.out.println("Added: " + rad + " " + px + " " + py);
+        par.add(tempPar);
+        index = count;
+        count++;
+	    System.out.println("Added: " + rad + " " + x + " " + y);
     }
 
+    public int getArrayLength() {
+        return par.size();
+    }
 
+    public int getCurrentParticleX(){
+        return par.get(index).getX();
+    }
+    public int getCurrentParticleY(){
+        return par.get(index).getY();
+    }
+    public int getCurrentParticleRad(){
+        return par.get(index).getRadius();
+    }
     // update state of each particle in list
     void update(int bw, int bh) {
+        int tempX = par.get(index).getX();
+        int tempY = par.get(index).getY();
+        int tempVelX = par.get(index).getVelocityX();
+        int tempVelY = par.get(index).getVelocityY();
+        int tempRad = par.get(index).getRadius();
+
+       /* par.get(index).setX(tempX + tempVelX);
+        par.get(index).setY(tempY + tempVelY);
+
+        if (tempX >= bw - tempRad && tempVelX > 0) {
+            par.get(index).setX(-tempVelX);
+        } else if (tempX < tempRad && tempVelX < 0) {
+            par.get(index).setX(-tempVelX);
+        }
+
+        if (tempY >= bh - tempRad && tempVelY > 0) {
+            par.get(index).setY(-tempVelY);
+        } else if (tempY < tempRad && tempVelY < 0) {
+            par.get(index).setY(-tempVelY);
+        }*/
+
+
+
+
+        /*
+        px += velX;
+	py += velY;
+
+	if (px >= bdWidth-radius && velX > 0) {
+	    velX = -velX;
+	}
+	else if (px < radius && velX < 0) {
+	    velX = -velX;
+	}
+
+	if (py >= bdHeight-radius && velY > 0) {
+	    velY = -velY;
+	}
+	else if (py < radius && velY < 0) {
+	    velY = -velY;
+	}*/
     }
 }
--- src/PaSkCode/PSysView.java
+++ src/PaSkCode/PSysView.java
@@ -1,20 +1,30 @@
 package PaSkCode;
 import java.awt.*;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class PSysView {
 
+
     PSysView() {
+
     }
 
     // draw all particles in psm
     void draw(PSysModel psm, Graphics g) {
+
+        int px = psm.getCurrentParticleX();
+        int py = psm.getCurrentParticleY();
+        int radius = psm.getCurrentParticleRad();
+
         Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHints(
-		    new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,
-				       RenderingHints.VALUE_TEXT_ANTIALIAS_ON));
-	    g2d.setColor(Color.RED);
-        //todo: to draw the circle with the data
-	    //g2d.fillOval(px-radius, py-radius, radius*2, radius*2);
+            g2d.setRenderingHints(
+                    new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,
+                            RenderingHints.VALUE_TEXT_ANTIALIAS_ON));
+            g2d.setColor(Color.RED);
+            //todo: to draw the circle with the data
+            g2d.fillOval(px - radius, py - radius, radius * 2, radius * 2);
+
     }
 
     // dump information on all particles in psm
